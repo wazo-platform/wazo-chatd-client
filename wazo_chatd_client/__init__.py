126cf21b312e546e0ba011fea4922d5dc5f58bac
Francois Blackburn <fblackburn@wazo.community>
2019-01-15 13:21:44 -0500

    allow import Client from __init__ module

---
+++ wazo_chatd_client/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from wazo_chatd_client.client import ChatdClient as Client
+
+__all__ = [
+    'Client',
+]
