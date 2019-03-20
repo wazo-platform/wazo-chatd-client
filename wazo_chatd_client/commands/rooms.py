053f46c6f0061c0e064175b108aecbf1a108b9e0
Francois Blackburn <fblackburn@wazo.community>
2019-03-20 08:40:45 -0400

    add room message commands

--- wazo_chatd_client/commands/rooms.py
+++ wazo_chatd_client/commands/rooms.py
@@ -21,3 +21,17 @@ class RoomCommand(BaseCommand):
         r = self.session.post(url, json=room_args, headers=headers)
         self.raise_from_response(r)
         return r.json()
+
+    def list_messages_from_user(self, room_uuid, **params):
+        headers = self._get_headers()
+        url = '{base}/{room_uuid}/messages'.format(room_uuid=room_uuid, base=self.base_url)
+        r = self.session.get(url, headers=headers, params=params)
+        self.raise_from_response(r)
+        return r.json()
+
+    def create_message_from_user(self, room_uuid, message_args):
+        headers = self._get_headers(write=True)
+        url = '{base}/{room_uuid}/messages'.format(room_uuid=room_uuid, base=self.base_url)
+        r = self.session.post(url, json=message_args, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
