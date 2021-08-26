ba6fe15f6c6c6d60905a8c35a132ed59e40f1695
Francois Blackburn <fblackburn@wazo.community>
2021-08-26 09:04:24 -0400

    update copyright

--- wazo_chatd_client/commands/config.py
+++ wazo_chatd_client/commands/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .helpers.base import BaseCommand
