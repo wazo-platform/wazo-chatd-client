86d053cde5f7e79228b3109c234f6ab8c5bc7937
Admin <admin@SJ-MacBook-Air.lan>
2022-10-29 16:19:01 +0330

    add room set activity command

--- wazo_chatd_client/commands/rooms.py
+++ wazo_chatd_client/commands/rooms.py
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .helpers.base import BaseCommand
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class RoomCommand(BaseCommand):
@@ -37,6 +40,8 @@ class RoomCommand(BaseCommand):
             room_uuid=room_uuid, base=self.base_url
         )
         r = self.session.post(url, json=message_args, headers=headers)
+        logger.warning('------message-test------')
+        logger.warning(r)
         self.raise_from_response(r)
         return r.json()
 
@@ -44,5 +49,16 @@ class RoomCommand(BaseCommand):
         headers = self._get_headers()
         url = '{base}/messages'.format(base=self.base_url)
         r = self.session.get(url, headers=headers, params=params)
+        self.raise_from_response(r)
+        return r.json()
+
+    def set_activity_from_user(self, room_uuid, activity_args):
+        headers = self._get_headers()
+        url = '{base}/{room_uuid}/activities'.format(
+            room_uuid=room_uuid, base=self.base_url
+        )
+        logger.warning('------activity-test------')
+        r = self.session.post(url, json=activity_args, headers=headers)
+        logger.warning(r)
         self.raise_from_response(r)
         return r.json()
