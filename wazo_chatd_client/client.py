5100888d8e3e8e3c246c12d55c440bc9d09fbdc5
Jesse Sopel <jesse.sopel@wazo.io>
2023-04-12 11:01:06 -0400

    Run black, since it has been updated

--- wazo_chatd_client/client.py
+++ wazo_chatd_client/client.py
@@ -1,11 +1,10 @@
-# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
 
 
 class ChatdClient(BaseClient):
-
     namespace = 'wazo_chatd_client.commands'
 
     def __init__(self, host, port=443, prefix='/api/chatd', version='1.0', **kwargs):
--- wazo_chatd_client/commands/status.py
+++ wazo_chatd_client/commands/status.py
@@ -1,11 +1,10 @@
-# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .helpers.base import BaseCommand
 
 
 class StatusCommand(BaseCommand):
-
     resource = 'status'
 
     def get(self):
