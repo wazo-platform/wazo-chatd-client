83fc1fcfe84a86c8c10aa2b92caa96ec5d089aa2
Julien Alie <jalie@wazo.io>
2026-04-08 11:21:23 -0400

    Add UserIdentity command for identity management endpoints
    - list, get, create, update, delete scoped to user_uuid
    - Optional tenant_uuid for cross-tenant management

---
+++ wazo_chatd_client/commands/user_identities.py
@@ -0,0 +1,41 @@
+# Copyright 2026 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .helpers.base import BaseCommand
+
+
+class UserIdentityCommand(BaseCommand):
+    resource = 'users'
+
+    def list(self, user_uuid, tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        url = f'{self.base_url}/{user_uuid}/identities'
+        r = self.session.get(url, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
+
+    def get(self, user_uuid, identity_uuid, tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        url = f'{self.base_url}/{user_uuid}/identities/{identity_uuid}'
+        r = self.session.get(url, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
+
+    def create(self, user_uuid, identity_args, tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        url = f'{self.base_url}/{user_uuid}/identities'
+        r = self.session.post(url, json=identity_args, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
+
+    def update(self, user_uuid, identity_uuid, identity_args, tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        url = f'{self.base_url}/{user_uuid}/identities/{identity_uuid}'
+        r = self.session.put(url, json=identity_args, headers=headers)
+        self.raise_from_response(r)
+
+    def delete(self, user_uuid, identity_uuid, tenant_uuid=None):
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        url = f'{self.base_url}/{user_uuid}/identities/{identity_uuid}'
+        r = self.session.delete(url, headers=headers)
+        self.raise_from_response(r)
